f98fd5aafff5678bf49ec18e5eb033328a27b21d
Bektur Soltobaev <b.soltobaev@disoft.dev>
2021-10-22 21:27:41 +0600

    Set up trip model

--- trains/trips/models.py
+++ trains/trips/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Trip(models.Model):
+    stations = models.ManyToManyField('directory.Station', through='TripStation')
+    train = models.ForeignKey('machines.Machine', on_delete=models.PROTECT, related_name='trips')
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+
+class TripStation(models.Model):
+    station = models.ForeignKey('directory.Station', on_delete=models.CASCADE, related_name='trip_station_links')
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name='trip_station_links')
+    arrival_time = models.DateTimeField()
+    queue = models.PositiveIntegerField()
