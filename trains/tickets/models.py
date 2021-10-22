26929fd0e800baf5bfb594e9b9fa79dc2b578c5b
Bektur Soltobaev <b.soltobaev@disoft.dev>
2021-10-22 21:38:00 +0600

    Set up tickets models

--- trains/tickets/models.py
+++ trains/tickets/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Ticket(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tickets')
+    trip = models.ForeignKey('trips.Trip', on_delete=models.PROTECT, related_name='tickets')
+    price = models.DecimalField(decimal_places=2, max_digits=10)
