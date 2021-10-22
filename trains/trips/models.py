0110eb83a57059e15876257f44b5caf87f40c3db
Bektur Soltobaev <b.soltobaev@disoft.dev>
2021-10-22 21:31:37 +0600

    Add document string to trips

--- trains/trips/models.py
+++ trains/trips/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class Trip(models.Model):
+    """
+    Model for representing a trip of train from beginning to end
+    """
     stations = models.ManyToManyField('directory.Station', through='TripStation')
     train = models.ForeignKey('machines.Machine', on_delete=models.PROTECT, related_name='trips')
     departure_time = models.DateTimeField()
@@ -9,6 +12,9 @@ class Trip(models.Model):
 
 
 class TripStation(models.Model):
+    """
+    Link model for stations-trips relation. Represents stops of the train
+    """
     station = models.ForeignKey('directory.Station', on_delete=models.CASCADE, related_name='trip_station_links')
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name='trip_station_links')
     arrival_time = models.DateTimeField()
